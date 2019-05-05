d2886bed6ad7767461399cfc15a9d26b032f37f9
Ekundayo Abiona <ekundayoabiona@gmail.com>
2019-05-05 20:47:21 +0100

    Change maps package

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,29 +1,35 @@
 import React from 'react'
-import { Map, GoogleApiWrapper } from 'google-maps-react'
+import GoogleMapReact from 'google-map-react';
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-const mapStyles = {
-  width: '100%',
-  height: '600px'
-};
-
-export const MapView = ({ lat, lon, google }) => {
+export const MapView = ({ lat, lon }) => {
   return (
     <div className="row">
       <MapView.Container className="col-12 p-0">
-          <Map
-            google={google}
-            zoom={14}
-            style={mapStyles}
-            initialCenter={{ lat, lng: lon }}
-          />
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }} //TODO: This should be in an environment file
+          defaultCenter={{ lat: parseFloat(lat), lng: parseFloat(lon) }}
+          defaultZoom={11}
+          options={(maps) => ({
+            mapTypeId: maps.MapTypeId.ROADMAP,
+            zoomControl: true,
+            mapTypeControl: true,
+            scaleControl: true,
+            streetViewControl: true,
+            rotateControl: true,
+            fullscreenControl: true
+          })}>
+        </GoogleMapReact>
       </MapView.Container>
     </div>
   )
 };
 
 MapView.Container = styled.div`
+  width: 100%;
+  height: 600px;
+
   &> div {
     position: inherit !important;
   }
@@ -34,7 +40,4 @@ MapView.propTypes = {
   lon: PropTypes.string.isRequired
 }
 
-export default GoogleApiWrapper({
-  //TODO: This should be in an environment file
-  apiKey: process.env.REACT_APP_GOOGLE_API_KEY
-})(MapView)
+export default MapView
