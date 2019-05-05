e6802d6fba986146f14c66d6e6485507f78e5668
Ekundayo Abiona <ekundayoabiona@gmail.com>
2019-05-05 12:54:17 +0100

    - Add bootstrap and styled-components packages - Add submission data to ease development locally - Setup main page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="container-fluid m-0 p-0">
+        <nav className="navbar navbar-light bg-light text-center">
+          <a className="navbar-brand" href="#!">Fotoquest Submission Evaluator</a>
+        </nav>
+      </div>
+
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-lg-12 col-12">
+            <SubmissionCard className="submission-card">
+              Hello World!
+            </SubmissionCard>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
-}
+};
+
+const SubmissionCard = styled.div`
+  background: #FFFFFF;
+`;
 
 export default App;
